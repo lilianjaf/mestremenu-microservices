171239641ab594ab592080c47a10a1b1b6c9ea31
Lilian Ferreira <lilianjaf@gmail.com>
2026-04-09 01:58:11 -0300

    chore: update Spring Cloud version to 2024.0.0 and enable OpenFeign in PagamentoService

--- pagamento-service/src/main/java/com/github/lilianjaf/pagamento_service/PagamentoServiceApplication.java
+++ pagamento-service/src/main/java/com/github/lilianjaf/pagamento_service/PagamentoServiceApplication.java
@@ -2,6 +2,7 @@ package com.github.lilianjaf.pagamento_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
