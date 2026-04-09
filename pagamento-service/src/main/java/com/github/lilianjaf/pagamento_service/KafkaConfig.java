9983025d0e99723297e9a9af2b9d5fd873a26a0c
Lilian Ferreira <lilianjaf@gmail.com>
2026-04-09 01:29:33 -0300

    feat: add Kafka topic configuration and enable OpenFeign in PagamentoService

---
+++ pagamento-service/src/main/java/com/github/lilianjaf/pagamento_service/KafkaConfig.java
@@ -0,0 +1,17 @@
+package com.github.lilianjaf.pagamento_service;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public NewTopic pagamentosTopic() {
+        return TopicBuilder.name("pagamentos-topic")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+}
